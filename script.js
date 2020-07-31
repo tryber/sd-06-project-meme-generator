82f317bc4967de61de6bd3b0e59d92f2de7efee4
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-07-31 16:49:18 -0300

    ajustes 1

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const textInput = document.querySelector('#text-input');
-const memeText = document.querySelector('#meme-text');
+window.onload = function () {
+
+    let container = document.getElementById("meme-image-container")
+    let caixaInput = document.getElementById("text-input")
+    
+    caixaInput.addEventListener("keyup", function (){
+      document.querySelector("#meme-text").innerHTML = caixaInput.value
+    })
+    
+    let image = document.querySelector("#meme-insert")
+    
+    image.addEventListener("input", function () {
+      document.getElementById("meme-image").src = image.value
+    })
+    }
