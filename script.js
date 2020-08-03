c4ee7623d582b7d29fa3a105f8098243edb38bf6
Lucas Miranda <lucasmirandadev@gmail.com>
2020-08-03 18:31:18 -0300

    adicionando imagem script

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ text.addEventListener('input',function(){
     let note = document.getElementById('meme-text');
     note.innerHTML = text.value;
 });
+
+let image = function (image_input) {
+    let output = document.getElementById('meme-image');
+    output.src = URL.createObjectURL(image_input.target.files[0]);
+    output.onload = function () {
+      URL.revokeObjectURL(output.src)
+    }
+};
