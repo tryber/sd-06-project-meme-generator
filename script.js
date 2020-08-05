d2475be078d405281bf0e53d8191b90e3683ed57
Lucas Miranda <lucasmirandadev@gmail.com>
2020-08-05 16:59:52 -0300

    atualizei html, css e script conforme visto no plantao

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-let text = document.getElementById('text-input');
-text.addEventListener('input',function(){
-    let note = document.getElementById('meme-text');
-    note.innerHTML = text.value;
+const inputImage = document.querySelector('#meme-insert');
+const memeImage = document.querySelector('#meme-image');
+const inputText = document.querySelector('#text-input');
+const memeText = document.querySelector('#meme-text');
+
+inputImage.addEventListener('change', function (event) {
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
 });
 
-
+inputText.addEventListener('insert', function () {
+  memeText.innerText = inputText.value;
+  memeText.className = 'text-input';
+});
