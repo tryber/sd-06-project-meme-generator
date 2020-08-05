2b184930c4f76245df3c1badb48145b1ab53a0e4
Lucas Miranda <lucasmirandadev@gmail.com>
2020-08-05 16:49:12 -0300

    retirei o script

--- script.js
+++ script.js
@@ -5,14 +5,3 @@ text.addEventListener('input',function(){
 });
 
 
-const photoInsert = document.getElementById('meme-insert')
-const memeImage = document.getElementById('meme-image')
-
-
-photoInsert.addEventListener('change',function(){
-    memeImage.src = URL.createObjectURL(event.target.files[0]);
-    memeImage.style.maxWidth ='100%';
-    memeImage.onload = function(){
-        URL.revokeObjectURL(memeImage.src)
-    }
-});
