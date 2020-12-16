b91456eff03df4e63b36027a93df16cb3e858889
VitorSSMoreira <vitorssmoreira@gmail.com>
2020-12-16 20:45:32 -0300

    requisito bonus

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-let loadImage = function (event) {
+function loadImage(event) {
   let output = document.getElementById('meme-image');
   output.src = URL.createObjectURL(event.target.files[0]);
-  output.onload = function () {
+  output.onload = function() {
     URL.revokeObjectURL(output.src) // free memory
   }
 };
 
+function memeChangeBorder(borderType) {
+  document.getElementById('meme-image-container').className=`border-${borderType}`;
+}
+
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
+const fireBtn = document.getElementById('fire');
+const waterBtn = document.getElementById('water');
+const earthBtn = document.getElementById('earth');
+
 textInput.addEventListener('input', memeTextInsert);
+fireBtn.addEventListener('click', () => memeChangeBorder('fire'));
+waterBtn.addEventListener('click', () => memeChangeBorder('water'));
+earthBtn.addEventListener('click', () => memeChangeBorder('earth'));
 
 function memeTextInsert() {
   memeText.innerHTML = textInput.value;
