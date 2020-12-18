bf829f4acf3e009866d1bc3afd2b9db30779eef5
VitorSSMoreira <vitorssmoreira@gmail.com>
2020-12-18 18:52:44 -0300

    requisito bonus

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+/** @format */
+
 function loadImage(event) {
   let output = document.getElementById('meme-image');
   output.src = URL.createObjectURL(event.target.files[0]);
-  output.onload = function() {
-    URL.revokeObjectURL(output.src) // free memory
-  }
-};
+  output.onload = function () {
+    URL.revokeObjectURL(output.src); // free memory
+  };
+}
+
+function loadPreLoadImage(srcURL) {
+  document.getElementById('meme-image').src = srcURL;
+}
 
 function memeChangeBorder(borderType) {
-  document.getElementById('meme-image-container').className=`border-${borderType}`;
+  document.getElementById('meme-image-container').className = `border-${borderType}`;
 }
 
 const textInput = document.getElementById('text-input');
